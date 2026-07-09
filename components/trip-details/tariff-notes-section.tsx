1d8301fc979dcc0ed76a562abcd5173f83475c1c
Mohamed Afser <mohamedafser2203@gmail.com>
2026-07-09 20:30:21 +0530

    eas setup

--- components/trip-details/tariff-notes-section.tsx
+++ components/trip-details/tariff-notes-section.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import AppModal from "../common/app-modal";
 import { SectionBlock } from "./section-block";
 
@@ -11,7 +11,9 @@ const TariffNotesSection = ({ notes }: TariffNotesSectionProps) => {
 
   return (
     <SectionBlock title="Tariff Notes">
-      <Text className="text-sm leading-6 text-zinc-700 mb-5">{notes}</Text>
+      <Text className="text-sm leading-6 text-zinc-700 mb-5 line-clamp-5">
+        {notes}
+      </Text>
       <TouchableOpacity className="self-start rounded-full border border-zinc-900 px-5 py-2 mb-5">
         <Text
           className="text-sm font-semibold text-zinc-950"
@@ -28,7 +30,9 @@ const TariffNotesSection = ({ notes }: TariffNotesSectionProps) => {
         onClose={() => setOpen(false)}
         showCloseIcon={true}
       >
-        <Text className="text-sm leading-6 text-zinc-700 mb-5">{notes}</Text>
+        <View className="max-h-[500px] overflow-y-auto">
+          <Text className="text-sm leading-6 text-zinc-700 mb-5">{notes}</Text>
+        </View>
       </AppModal>
     </SectionBlock>
   );
